94f8b3e31d7f747029840be320457c99b823ec5e
sunny kumar <81152987+sunnykumar44@users.noreply.github.com>
2026-05-19 12:19:40 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+set -e
+
 echo "Starting deployment..."
 
-echo "Connecting to MariaDB..."
+echo "Connecting to MariaDB server: $DB_HOST"
 
-mysql -h "$DB_HOST" \
+mysql \
+-h "$DB_HOST" \
 -u "$DB_USER" \
 -p"$DB_PASS" \
 -e "SHOW DATABASES;"
